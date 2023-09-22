40f6a41c459586bdce1594f7a1da537db6d18f05
MarcoDiazLara <marco.diazl@alumno.buap.mx>
2023-09-22 17:56:07 -0600

    puto dani

---
+++ src/app/Inmueble/inmueble.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-inmueble",
+  templateUrl: "./inmueble.component.html",
+  styleUrls: ["./inmueble.component.css"],
+})
+export class inmuebleComponent {}
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-aut",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
+})
+export class authComponent {}
