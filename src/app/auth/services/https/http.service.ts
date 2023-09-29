af27568d646382c1505ee976a2ec0e7d953ae4e8
MarcoDiazLara <marco.diazl@alumno.buap.mx>
2023-09-29 17:14:06 -0600

    registro de usuario y login funcional

--- src/app/auth/services/https/http.service.ts
+++ src/app/auth/services/https/http.service.ts
@@ -14,4 +14,15 @@ export class HttpService {
     return this.httpclient.post(this.url + 'consultar_login.php', params, { headers });
   }
 
+
+  //p_nombres,p_a_paterno,p_a_materno,p_nom_usuario,p_contrasena,p_correo,p_tel_fijo,p_cel
+  registroCompleto(p_nombres: string,p_a_paterno: string,p_a_materno: string
+    ,p_nom_usuario: string,p_contrasena: string,p_correo:string,p_tel_fijo:string,p_cel: string){
+    let headers: any = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    let params = 'p_nombres='+p_nombres+'&p_a_paterno='+p_a_paterno+
+    '&p_a_materno='+p_a_materno+'&p_nom_usuario='+p_nom_usuario+'&p_contrasena='+p_contrasena
+    +'&p_correo='+p_correo+'&p_tel_fijo='+p_tel_fijo+'&p_cel='+p_cel;
+    return this.httpclient.post(this.url + 'insertarUsuario.php', params, { headers });
+  }
+
 }
